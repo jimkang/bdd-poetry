2b212640564e3282778520e047e8125580a2c73a
Jim Kang <jimkang@gmail.com>
2015-07-07 22:23:46 -0400

    Deduped data. Converting past tense in THEN to present.

--- tests/basictests.js
+++ tests/basictests.js
@@ -5,6 +5,14 @@ var testCases = [
   {
     poem: 'His death poem:\n\n        A bath when you\'re born,\n        a bath when you die,\n        how stupid.',
     expected: 'Scenario: His death\nGIVEN:         A bath when you\'re born\nWHEN:         a bath when you die\nTHEN:         how stupid.'
+  },
+  {
+    poem: "In the thicket's shade\na woman by herself\nsinging the rice-planting song.",
+    expected: "GIVEN: In the thicket's shade\nWHEN: a woman by herself\nTHEN: sing the rice-planting song."
+  },
+  {
+    poem: "Napped half the day;\nno one \npunished me!",
+    expected: "GIVEN: Napped half the day\nWHEN: no one\nTHEN: punish me!"
   }
 ];
 
